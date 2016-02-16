95d4776040acc4f54b1d701e4b372292947e6942
Don Richards <donrichards@outlook.com>
2016-02-16 17:21:42 -0500

    Remove Node_modules
    Remove

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -14,11 +14,10 @@ import Dropzone from 'react-dropzone';
 
 
 class Upload extends Component {
-  onDrop: function (files) {
-        console.log('Received files: ', files);
-      },
+
   render() {
     return (
+      <div>
         <Dropzone onDrop={this.onDrop}>
           <div>Try dropping some files here, or click to select files to upload.</div>
         </Dropzone>
