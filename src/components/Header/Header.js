146596f90c8cca798380dea2bb27d1fac6a704c9
Don Richards <donrichards@outlook.com>
2016-02-17 13:14:43 -0500

    Change GUI Theme
    Change theme items to main index page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,3 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
 
 import React, { Component } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
@@ -21,12 +13,12 @@ class Header extends Component {
         <div className={s.container}>
           <Navigation className={s.nav} />
           <Link className={s.brand} to="/">
-            <img src={require('./logo-small.png')} width="38" height="38" alt="React" />
-            <span className={s.brandTxt}>Your Company</span>
+            <img src="./hops.png" width="38" height="38" alt="ibu" />
+            <span className={s.brandTxt}>UTK</span>
           </Link>
           <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
+            <h1 className={s.bannerTitle}>Ingestion Batch Utility</h1>
+            <p className={s.bannerDesc}>Sprint to Ingestion Batch Utility for automating, validating Objects and ingestion</p>
           </div>
         </div>
       </div>
