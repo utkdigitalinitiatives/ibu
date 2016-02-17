dc035414129372f564446b836f2254f16cd47967
pc37utn <pc37utn@users.noreply.github.com>
2016-02-17 16:34:05 -0500

    Update ingestion.js

--- src/tap/ingestion.js
+++ src/tap/ingestion.js
@@ -22,11 +22,12 @@ fs.readdir(path, function(err, items) {
     }
 });
 // build command pieces
-let serveruri = 'http://localhost';
+let serveruri = 'http://dlwork.lib.utk.edu/dev/';
 let contentmodel = 'islandora:sp_basic_image';
 let parentpid = '';
 let namespace = '';
-let target = '/gwork/user/sanborn-ingest';
+// target is the local directory holding the ingest files
+let target = '';
 // execute drush command
 var exec = require('child_process').exec;
 var cmd = 'drush -v --user=user --uri='.serveruri.' ibsp --content_models='.contentmodel.' --type=directory --parent='.parentpid.' --namespace='.namespace.' --target=-v --user=user --uri=http://localhost ibsp --content_models=islandora:sp_basic_image --type=directory --parent=collections:sanborn --namespace=sanborn --target='target;
