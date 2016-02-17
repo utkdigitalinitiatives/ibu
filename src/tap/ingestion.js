e3c79bb1f28e856cf299f3d67d3968415a7c9867
pc37utn <pc37utn@users.noreply.github.com>
2016-02-17 14:26:19 -0500

    add listing current files in directory

--- src/tap/ingestion.js
+++ src/tap/ingestion.js
@@ -6,3 +6,20 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
  */
+var fs = require('fs');
+
+
+if (process.argv.length <= 2) {
+    console.log("Usage: " + __filename + ".");
+    process.exit(-1);
+}
+
+var path = process.argv[2];
+
+fs.readdir(path, function(err, items) {
+    console.log(items);
+
+    for (var i=0; i<items.length; i++) {
+        console.log(items[i]);
+    }
+});
