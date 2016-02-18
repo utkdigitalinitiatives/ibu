b10461b7fbfdc448185aa47989606289a8c9c873
cdeaneGit <cdeane@utk.edu>
2016-02-18 15:12:34 -0500

    Add header comments to top of file.

---
+++ src/tap/status.js
@@ -0,0 +1,22 @@
+/**
+ * status.js
+ * chd 160218
+ *
+ * status: 
+ * function inputs:  
+ *    1. error.txt array of strings from XMLvalidataion  
+ *    2. error.txt array of strings from IMGvalidataion  
+ *    3. error.txt array of strings from Controller_validataion  
+ * function outputs:  
+ *    1. errorReport.txt flat file written to a directory or sent to Controller
+ *    2. errorLog.txt flat file written to a directory or sent to Controller
+ * function purpose:  
+ *    1. Translate coded input messages to English Language format
+ *	 for errorReport.txt
+ *    2. Translate coded input messages to errorLog format for errorLog.txt
+ *
+ *
+ *
+ *
+ * @return {[type]} [description]
+ */
