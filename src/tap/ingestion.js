6f97712c0df84d664ff3b08e6c79755992418cf1
pc37utn <pc37@utk.edu>
2016-02-24 13:47:34 -0500

    remove linefeed from console log statement, what kind of screen processor does not interpret linefeeds?.

--- src/tap/ingestion.js
+++ src/tap/ingestion.js
@@ -36,7 +36,7 @@ function ingestion(target,parentpid,namespace,model) {
   // build command pieces
   // serveruri is the location of the drupal_home on the drupal server
   var drupalhome = '/vhosts/dlwork/web/collections';
-  console.log('drupalhome = '.drupalhome.'\n');
+  console.log('drupalhome = '.drupalhome);
   let serveruri = 'http://dlwork.lib.utk.edu/dev/';
   console.log('serveruri = '.serveruri.'\n');
   console.log('parentpid = '.parentpid.'\n');
