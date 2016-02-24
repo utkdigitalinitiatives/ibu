6c9b5865aeb2262bc61550c995d294f76d112321
CanOfBees <bdysonsmith@gmail.com>
2016-02-24 09:59:42 -0500

    drop unnecessary paths comment

--- src/tap/XMLvalidation.js
+++ src/tap/XMLvalidation.js
@@ -5,19 +5,3 @@
  *
  */
 
-/**
- * a list of paths for vars ([O)ptional|(R)equired]-[(Y)es|(N)o Repeatable?])
- *
- * abstract = modsIn["mods"]["abstract"] (O-Y)
- * coll = modsIn["mods"]["relatedItem"]["displayLabel"]["Collection"]["type"]["host"]["titleInfo"]["title"](R-N; Req'd if available)
- * collIdentifier = modsIn["mods"]["relatedItem"]["displayLabel"]["Collection"]["type"]["host"]["identifier"](R-N; Req'd if available)
- * dateCreated = modsIn["mods"]["originInfo"]["dateCreated"](R-R)
- * dateCreatedKey = modsIn["mods"]["originInfo"]["dateCreated"]["encoding"]["edtf"]["keyDate"]["yes"]["point"]["start"](R-N)
- * dateQualifier = modsIn["mods"]["originInfo"]["dateCreated"]["keyDate"]["yes"]["qualifier"]["..."](R-N; Req'd if available)
- * datePublication = modsIn["mods"]["originInfo"]["dateIssued"](O-N)
- * digitalOrigin = modsIn["mods"]["physicalDescription"]["digitalOrigin"](R-N)
- * extent = modsIn["mods"]["physicalDescription"]["extent"](O-N)
- * identifierFile = modsIn["mods"]["identifier"]["type"]["file"](R-Y)
- * identifierLocal = modsIn["mods"]["identifier"]["type"]["local"]...
- *
- */
