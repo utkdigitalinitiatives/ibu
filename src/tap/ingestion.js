1d48ddbb67fe463f328a20daed55167d459a2fea
pc37utn <pc37@utk.edu>
2016-02-24 13:44:09 -0500

    change let to var because of crash.

--- src/tap/ingestion.js
+++ src/tap/ingestion.js
@@ -35,7 +35,7 @@ model = String(param[5]);
 function ingestion(target,parentpid,namespace,model) {
   // build command pieces
   // serveruri is the location of the drupal_home on the drupal server
-  let drupalhome = '/vhosts/dlwork/web/collections';
+  var drupalhome = '/vhosts/dlwork/web/collections';
   console.log('drupalhome = '.drupalhome.'\n');
   let serveruri = 'http://dlwork.lib.utk.edu/dev/';
   console.log('serveruri = '.serveruri.'\n');
