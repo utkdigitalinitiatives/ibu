f4024a398aa045d09891072e690f35635e15137a
pc37utn <pc37@utk.edu>
2016-02-24 12:06:45 -0500

    add console print of serveruri.

--- src/tap/ingestion.js
+++ src/tap/ingestion.js
@@ -38,6 +38,7 @@ function ingestion(target,parentpid,namespace,model) {
   let drupalhome = '/vhosts/dlwork/web/collections';
   console.log('drupalhome = '.drupalhome.'\n');
   let serveruri = 'http://dlwork.lib.utk.edu/dev/';
+  console.log('serveruri = '.serveruri.'\n');
   console.log('parentpid = '.parentpid.'\n');
   // namespace
   console.log('namespace = '.namespace.'\n');
