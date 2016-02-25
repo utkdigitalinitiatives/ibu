ed37b0d7f41e357fef96b0680322c77d4fe9f842
pc37utn <pc37@utk.edu>
2016-02-25 14:20:57 -0500

    comment end of else.

--- src/tap/ingestion.js
+++ src/tap/ingestion.js
@@ -92,7 +92,7 @@ function ingestion(target,parentpid,namespace,model) {
      $message = 'parameters for first command missing, ingest not started.';
      status.push("$message");
      return $message;
-  }
+  }// end else
   // exec second drush command
   var cmd2 = String('drush -r '+drupalhome+'-v -u=1 --uri='+serveruri+' islandora_batch_ingest');
   console.log('cmd2=',cmd2);
