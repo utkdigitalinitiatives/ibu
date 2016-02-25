560ed249fd368d3daf53e7ab385c9f121456b4b9
bridger <bdysonsmith@gmail.com>
2016-02-25 16:53:37 -0500

    start fixing mistakes in how I'm handling arrays.
    face + palm.

--- src/tap/XMLvalidation.js
+++ src/tap/XMLvalidation.js
@@ -23,29 +23,28 @@ var fileKey = fileID.slice(0, -4);
 
 // abstract (optional and repeatable)
 // MS/AR collection title test (req'd if available, non-repeating)
-if (jp.query(modsObj, '$.mods.relatedItem[?(@.displayLabel=="Collection")].titleInfo.title') > 1) {
+var collTitle = jp.query(modsObj, '$.mods.relatedItem[?(@.displayLabel=="Collection")].titleInfo.title');
+if (collTitle.length > 0) {
+  console.log('Too many relatedItem[@displayLabel=\'Collection\']/titleInfo/title elements');
+  status.push('Too many relatedItem[@displayLabel=\'Collection\']/titleInfo/title elements');
+}
+if (jp.query(modsObj, '$.mods.relatedItem[?(@.displayLabel=="Collection")].titleInfo.title') > 0) {
   console.log('Too many collection titles');
   status.push('Too many collection titles');
 }
 // MS/AR collection identifier test (req'd if available, non-repeating)
 if (jp.query(modsObj, '$.mods.relatedItem[?(@.displayLabel=="Collection")].identifier') > 1) {
-  console.log('Too many MS/AR identifiers');
   status.push('Too many MS/AR identifiers');
 }
 // originInfo/dateCreated test (required and repeatable)
 // @TODO clean up this test; should be at least 1 dateCreated w/ no keyDate
 if (!jp.query(modsObj, '$.mods.originInfo.dateCreated[0][?(@.keyDate)]')) {
-  console.log('Missing originInfo/dateCreated');
   status.push('Missing originInfo/dateCreated');
-} else {
-  console.log('Has an originInfo/dateCreated');
 }
 // originInfo/dateCreated[@keyDate][@point='start'] (req'd, non-repeating)
 // @TODO combine [@qualifier] test here?
 if (!jp.query(modsObj, '$.mods.originInfo.dateCreated[?(@.keyDate && @.point=="start")]' || jp.query(modsObj, '$.mods.originInfo.dateCreated[?(@.keyDate && @.point=="start")]') > 1)) {
-  console.log('Too many keyDate starting points');
-} else {
-  console.log('keyDate test passed');
+  status.push('Too many keyDate starting points');
 }
 // originInfo/dateCreated[@keyDate][@qualifier] (req'd if available, non-repeating)
 
@@ -56,7 +55,7 @@ if (!jp.query(modsObj, '$.mods.originInfo.dateIssued') <= 1) {
 }
 // physicalDescription/digitalOrigin (required, non-repeating)
 if (jp.query(modsObj, '$.mods.physicalDescription.digitalOrigin') > 1) {
-  console.log('Too many digitalOrigin elements');
+  status.push('Too many digitalOrigin elements');
 }
 // physicalDescription/extent (optional, non-repeating)
 // @TODO logic problem
@@ -70,33 +69,65 @@ if (!jp.query(modsObj, '$.mods.physicalDescription.extent') <= 1) {
 
 // physicalDescription/form (required and repeatable)
 if (!jp.query(modsObj, '$.mods.physicalDescription.form')); {
-  console.log('Missing physicalDescription/form');
+  status.push('Missing physicalDescription/form');
 }
 // genre (optional and repeatable)
 // host title relatedItem[@type='host']/titleInfo/title (optional and repeatable)
 // physicalDescription/internetMediaType (required, non-repeating)
 if (jp.query(modsObj, '$.mods.physicalDescription.internetMediaType') > 1) {
-  console.log('Too many internetMediaTypes');
+  status.push('Too many internetMediaTypes');
 }
 // typeOfResource (required, non-repeating)
 var typeRes = jp.query(modsObj, '$.mods.typeOfResource');
 if ((typeRes > 1) || (typeRes = 0)) {
-  console.log('Problems with typeOfResource');
+  status.push('Problems with typeOfResource');
 }
 // language/languageTerm (optional, repeatable)
 // recordInfo/languageOfCataloging (required, non-repeating)
 var langTerm = jp.query(modsObj, '$.mods.recordInfo.languageOfCataloging.languageTerm["$t"]');
 if (String(langTerm) != "eng") {
-  console.log('Verify languageOfCataloging/languageTerm');
+  status.push('Verify languageOfCataloging/languageTerm');
 }
 if ((langTerm > 1) || (langTerm < 1)) {
-  console.log('Problems with languageTerm');
+  status.push('Problems with languageTerm');
 }
-// name/namePart
-// name[@authority]
-// name/namePart/role/roleTerm
-// ...
+// name/namePart (optional, repeatable)
+// name[@authority] (optional, repeatable)
+// name/namePart/role/roleTerm (req'd if available, repeatable)
+// name[@type] (optional, repeatable)
+// note[@type='ownership'] (optional, non-repeating)
+if (jp.query(modsObj, '$.mods.note[?(@.type=="ownership")]'))
 // publisher
+// recordInfo/recordOrigin (req'd, non-repeating)
+var recordOrigin = jp.query(modsObj, '$.mods.recordInfo.recordOrigin');
+if (jp.query(modsObj, '$.mods.recordInfo.recordOrigin') > 1) {
+  status.push('Too many recordOrigin elements');
+}
+// recordInfo/recordContentSource (optional, non-repeating)
+if (jp.query(modsObj, '$.mods.recordInfo.recordContentSource') > 1) {
+  status.push('Too man recordContentSource elements');
+}
+// repository, mods/location/physicalLocation (req'd if available, non-repeating)
+if ((jp.query(modsObj, '$.mods.location.physicalLocation') > 1) || (jp.query(modsObj, '$.mods.location.physicalLocation') < 1)) {
+  status.push('physicalLocation problem');
+}
+// rights (required, non-repeating)
+var rights = jp.query(modsObj, '$.mods.accessCondition');
+if ((rights > 1) || (rights < 1)) {
+  status.push('Rights problem');
+}
+// shelf_locator, location/holdingSimple/copyInformation/shelfLocator (optional, non-repeating)
+if (jp.query(modsObj, '$.mods.location.holdingSimple.copyInformation.shelfLocator') > 1) {
+  status.push('Too many shelfLocators');
+}
+// subjects (all optional, all repeatable)
+// thumbnail (req'd if applicable, non-repeating)
+// title_initial_article, titleInfo/nonSort: articles in the title element (req'd if applicable, repeatable)
+// title_of_part, titleInfo/partName (req'd if applicable, repeatable)
+// title (required, repeatable)
+if (!jp.query(modsObj, '$.mods.titleInfo.title') >= 1) {
+  console.log('Not enough titles');
+}
 
 
 
