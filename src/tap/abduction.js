09047c916a2408228652f63cb03ff2ec4c0888d5
Don Richards <donrichards@outlook.com>
2016-02-26 14:30:44 -0500

    Remove Recursive readdir
    Remove because of limitations

--- src/tap/abduction.js
+++ src/tap/abduction.js
@@ -1,6 +1,4 @@
 "use strict";
-
-let recursive = require('recursive-readdir');
 const fs = require('fs');
 
 /**
@@ -14,18 +12,39 @@ let search = 'delivery';
 let gravity = './test/';
 let path = require('path');
 let fileArray = [];
-
-console.log('fs: ', process.cwd() );
-
+//console.log('fs: ', process.cwd() );
 // Find File List within a folder
 module.exports = function() {
-  fileArray = deliveryDirectory();
+  deliveryDirectory();
+  setTimeout(waiting, 3000);
 };
 
 function deliveryDirectory(){
-  recursive(gravity, ['*.txt'], function (err, files) {
-      // Files is an array of filename
-      console.log(files);
-      return files;
-    });
- };
+  
+  fs.readdir(gravity , function (err, files) {
+      if (err) {
+          throw err;
+      }
+      files.map(function (file) {
+          return path.join(gravity, file);
+      }).filter(function (file) {
+          return fs.statSync(file).isFile();
+      }).forEach(function (file) {
+        if(path.extname(file)=='.xml'){
+          //console.log("MODS %s (%s)", file, path.extname(file));
+          fileArray.push(file);
+        }else if(path.extname(file)=='.tif'||path.extname(file)=='.jp2'){
+          fileArray.push(file);
+          //console.log("Image %s (%s)", file, path.extname(file));
+        };
+      });
+  });
+};
+
+function waiting(){
+  console.log('x: ', fileArray.length);
+  if(fileArray.length>0){
+  process.exit();
+  };
+  setTimeout(waiting, 3000);
+};
