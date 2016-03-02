8a8c431213a54b150596bdac5d2d8012b62d81e2
Don Richards <donrichards@outlook.com>
2016-03-02 08:34:12 -0500

    Add db Access

--- src/tap/abduction.js
+++ src/tap/abduction.js
@@ -1,50 +1,113 @@
-"use strict";
+
 const fs = require('fs');
+const path = require('path');
 
-/**
- * [abduction description]
- * @param  {[type]} input [Folder to scan]
- * @return {[type]}       [2 Arrays of file locations]
- *
- */
-
-let search = 'delivery';
+// Mongoose connection to MongoDB
+const mongoose = require('mongoose');
 let gravity = './test/';
-let path = require('path');
-let fileArray = [];
-//console.log('fs: ', process.cwd() );
-// Find File List within a folder
-module.exports = function() {
-  deliveryDirectory();
-  setTimeout(waiting, 3000);
-};
+const dbUri = 'mongodb://localhost:27017/fileList';
+mongoose.connect(dbUri);
 
-function deliveryDirectory(){
-  
-  fs.readdir(gravity , function (err, files) {
-      if (err) {
-          throw err;
-      }
+let userSchema = new mongoose.Schema({
+      filename: { type: String, trim: true },
+      extentionName: { type: String, trim: true },
+      error: [ String ]
+    });
+
+let ibuErrorDoc = mongoose.model('filelistings', userSchema);
+mongoose.connection.collections['filelistings'].drop();
+
+mongoose.connection.on('connected', function () {
+});
+mongoose.connection.on('error',function (err) {
+  console.log(err.stack);
+});
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose default connection disconnected');
+});
+
+let status = '';
+
+function abduction(callback) {
+    let readFiles = 0;
+    let readDirrectories = 0;
+    if(status=='success'){
+      callback = status;
+      return callback;
+    };
+    fs.readdir(gravity, function (err, files) {
+      if (!files.length) {
+        return console.log('No files to show');
+      };
       files.map(function (file) {
-          return path.join(gravity, file);
+        return path.join(gravity, file);
       }).filter(function (file) {
-          return fs.statSync(file).isFile();
+        if(fs.statSync(file).isFile()== false){
+          readDirrectories++;
+        }
+        return fs.statSync(file).isFile();
       }).forEach(function (file) {
-        if(path.extname(file)=='.xml'){
-          //console.log("MODS %s (%s)", file, path.extname(file));
-          fileArray.push(file);
-        }else if(path.extname(file)=='.tif'||path.extname(file)=='.jp2'){
-          fileArray.push(file);
-          //console.log("Image %s (%s)", file, path.extname(file));
+        readFiles++;
+        if(path.extname(file)=='.tif'||path.extname(file)=='.jp2'|| path.extname(file)=='.xml'){
+          saveToDB(file);
+          if( (readFiles+readDirrectories)===files.length) {
+            // console.log('Ready to Exit Read');
+           status = 'success';
+           abduction(status);
+          };
         };
       });
-  });
+    });
 };
 
-function waiting(){
-  console.log('x: ', fileArray.length);
-  if(fileArray.length>0){
-  process.exit();
-  };
-  setTimeout(waiting, 3000);
+//   let get_files = function (cb) {
+//     fs.readdir(gravity, function (err, files) {
+//         if (!files.length) {
+//             return console.log('No files to show');
+//         }
+//         let file_dict = {};
+//         let file_index = 0;
+//
+//         function file(i) {
+//             let filename = files[i];
+//             fs.stat(gravity + filename, function (err, stat) {
+//                 if (stat.isDirectory() || filename[0] == '.') {
+//                     // do nothing, skip these dictionaries and dot files
+//                 } else {
+//                     ++file_index;
+//                     file_dict[file_index] = filename;
+//                 };
+//
+//                 if (++i == files.length) {
+//                     console.log('right before returning');
+//                     console.log(file_dict);
+//                     return cb(file_dict);
+//                 } else {
+//                     // continue getting files
+//                     return file(i);
+//                 };
+//             });
+//         }
+//         return file(0);
+//     });
+// }
+// get_files(function (v) { console.log(v); });
+
+
+
+function saveToDB(file){
+  let fileFoundInFolder = new ibuErrorDoc ({
+      filename: path.resolve(file),
+      extentionName: path.extname(file)
+    });
+  fileFoundInFolder.save(function (err) {if (err) console.log ('Error on save!')});
 };
+
+function saveErrorToDB(errors){
+  let errorArray = new ibuErrorDoc ({
+      error: errors
+    });
+  errorArray.save(function (err) {if (err) console.log ('Trying to write to a db it has no access to! Error on save!')});
+};
+
+export default abduction;
--- src/tap/controller.js
+++ src/tap/controller.js
@@ -1,40 +1,64 @@
 'use strict';
-
-//import status from './status';
-let abduction = require('./abduction');
-
+// import status from './status';
+import abduction from './abduction';
 // import imgvalid from './IMGvalidation';
 // import xmlvalid from './XMLvalidation';
 // import ingest from './ingestion';
+const fs = require('fs');
 
-let mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/ibu');
-let db = mongoose.connection;
+// function img(abductionExport, callback){
+//   console.log('abductionCallBack: ', abductionExport);
+//   callback = 'success';
+//   return(null, callback);
+// };
 
-
-/**
- * [abduction Search for './delivery' and Get Complete File List]
- * @return {[String]} [Array of Strings]
- * @return {dataBaseCollection} [Single String of collection name]
- */
-
-
-function controller() {
-  console.log('Fired');
-  var dataBaseCollection = abduction();
-  console.log(dataBaseCollection);
-  // console.log('abduction: '+abduction+' type of: '+ typeof abduction);
-  // let imgStatus = imgvalid(dataBaseCollection);
-  // let xmlStatus = xmlvalid(dataBaseCollection);
-  //
-  // if(imgStatus[2]!='success'||xmlStatus[2]!='success'){
-  //   console.log('Failed? '+imgStatus[2]);
-  // }else if(imgStatus[2]==='success'&&xmlStatus[2]==='success'){
-  //   console.log('Success! '+'['+imgStatus[2]+', '+xmlStatus[2]+']');
-  // }else{
-  //   console.log('What just happened? Is this even possible?');
-  // }
+function controller(){
+  Promise.all([
+    abduction,
+    ingestionPrep
+  ]).then(function(values){
+      let abduct = values[0];
+      let ingestPrep= values[1];
+      console.log(abduct, ingestPrep);
+  });
+  // function xml(callback){console.log('xml');};
+  // function img(callback){console.log('img');};
+  // function drush(callback){console.log('drush');};
+  // function cleanup(callback){console.log('cleanup');};
 };
-// controller();
+
+    // takeAction.next().value;
+    // if(imgvalid=='success') {
+  //   takeAction.next().value};
+  //   }else{console.log(loadFileList)};
+  // if(xmlvalid=='success') {
+  //   takeAction.next().value};
+  //   }else{console.log(loadFileList)};
+  // if(ingestionPrep=='success') {
+  //   takeAction.next().value};
+  //   }else{console.log(loadFileList)};
+  // if(ingest=='success') {
+  //   takeAction.next().value};
+  //   }else{console.log(loadFileList)};
+// };
+
+function ingestionPrep(status){
+  if(fs.existsSync('./test/staging')){
+      console.log('Already Exist');
+      return status;
+  }else{
+    fs.mkdir('./test/staging', (err)=> {
+        if(err) {
+          console.log(err);
+          status = err;
+          return status;
+        }else{
+          console.log('directory Created');
+          return status;
+          };
+      });
+  };
+};
+
 
 export default controller;
