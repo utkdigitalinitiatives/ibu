0ea7667da177e969fe9dc0e04db977db50c9d4fa
Don Richards <donrichards@outlook.com>
2016-03-02 15:02:15 -0500

    Add Fields & removed Validate
    Remove validate until Schema is working on all code.
    Renamed "collection", conflict with core naming in Mongoose

--- src/tap/schema.js
+++ src/tap/schema.js
@@ -1,20 +1,31 @@
+'use strict';
 /**
  * [schema defines validation schemas for Mongo documents being inserted into db:ibu collection:ibuerrors]
  *
  *
  */
+ // Mongoose connection to MongoDB
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var ibuErrorSchema = new Schema({
   filename: {
     type: String,
-    validate: {
-      validator: function(v){
-        return /^[^.]+$/.test(v);
-      },
-      message: 'Filename is not valid!'
-    }
+    trim: true
   },
-  collection: {
+  filePathXML: {
+    type: String,
+    unique: true
+  },
+  filePathIMG: {
+    type: String,
+    unique: true
+  },
+  extentionName: {
+    type: String,
+    trim: true
+  },
+  libCollection: {
     type: String
   },
   IMGerrors: {
@@ -22,7 +33,15 @@ var ibuErrorSchema = new Schema({
   },
   XMLerrors: {
     type: Array
+  },
+  created: {
+    type: Date , default: Date.now
   }
 });
+//,
+// validate: {
+//   validator: function(v){
+//     return /^[^.]+$/.test(v);
 
 var ibuErrorDoc = mongoose.model('ibuErrorDoc', ibuErrorSchema);
+module.exports = ibuErrorDoc;
