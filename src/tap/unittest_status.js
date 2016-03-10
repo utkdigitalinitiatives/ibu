53d5590e604d4448e4e48aa11cdb642442704f90
cdeaneGit <cdeane@utk.edu>
2016-03-10 10:28:24 -0500

    Add capability to unittest_status.js.  Now the ErrorArray can be default values or from the command_line argument list.

--- src/tap/unittest_status.js
+++ src/tap/unittest_status.js
@@ -1,27 +1,47 @@
-/* chd 160307 unittest function call to status
- * command line example:
- * node status.js 0012_000251_000028_0001.jp2 "Cannot read file" "Incorrect file format" "no kittens" 
+/* chd 160310 unittest function call to status.translateArgs
  *
- * see http://www.scriptol.com/javascript/include.php
- * I used this one the most: * see http://www.scriptol.com/javascript/include.php
+ * command line example:
+ * using default ErrorArray inside the script.
+ * node unittest_status.js 
+ *
+ * command line example:
+ * using argument list on the command line.
+ * node unittest_status.js 0012_000251_000028_0001.jp2 "Cannot read file" "Incorrect file format" "no kittens" 
+ *
+ *
+ * reference: http://www.scriptol.com/javascript/include.php
  * 
  *
  */
 
-/* import translateArgs from './tstatus.js'; */
+
+var process = require('process');
+var argv = process.argv;
+var ErrorArray=[];
+	if (argv.length > 2) { /*build ErrorArray from command_line*/
+		var i  = 0;
+		var ct = 2; 
+		while (ct < argv.length){
+			ErrorArray[i] = argv[ct];
+			//console.log ("ErrorArray["+i+"]="+ErrorArray[i]);
+			i  = i  +1;
+			ct = ct +1;
+		}/*end while*/
+ 
+	}else {/*use default ErrorArray values*/
+		ErrorArray = ["0012_000251_000028_0001.jp2"
+            ,"Cannot read file" 
+			,"Incorrect file format" 
+			,"no kittens" ];
+			
+	}/*end if-else*/
+
 
 var vm = require('vm');
 var fs = require('fs');
 var Fname = "./status.js"
 
 vm.runInThisContext(fs.readFileSync(Fname));
-
-
-var ErrorArray = ["0012_000251_000028_0001.jp2"
-            ,"Cannot read file" 
-			,"Incorrect file format" 
-			,"no kittens" ];
-			
 
 var returnString = translateArgs(ErrorArray);
  console.log(returnString);
