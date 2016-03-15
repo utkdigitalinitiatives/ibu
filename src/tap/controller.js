22be3478e640c43c7271a08b463c32143071a765
Don Richards <donrichards@outlook.com>
2016-03-15 10:27:15 -0400

    Revise Promise
    Non working Promise setup

--- src/tap/controller.js
+++ src/tap/controller.js
@@ -1,83 +1,76 @@
-const fs = require('fs');
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
 const IbuErrorDoc = require('./schema');
-
 /*eslint-disable*/
 const db = require('../config/db');
 /*eslint-enable*/
-
 // import * as status from './status';
 import abduction from './abduction';
 // import imgvalid from './IMGvalidation';
 // import xmlvalid from './XMLvalidation';
-// target = '/home/vagrant/imagetest';
-// parentpid = 'islandora;test2';
-// namespace = 'test2';
-// model = 'basic';
-let target = '/home/vagrant/imagetest';
-let parentpid = 'islandora:test2';
-let namespace = 'test2';
-let model = 'basic';
-import ingest from './ingestion';
+// let target = '/home/vagrant/imagetest';
+// let parentpid = 'islandora:test2';
+// let namespace = 'test2';
+// let model = 'basic';
+// import ingest from './ingestion';
 
 const gravity = './test/';
-let steps = 0;
-function ingestionPrep(status) {
+
+function ingestionPrep(resolve, reject) {
   if (fs.existsSync('./test/staging')) {
-      // console.log('Already Exist');
-    return status;
+    return resolve('success');
   } else { //eslint-disable-line
     fs.mkdir('./test/staging', (err) => {
       if (err) {
-        console.log(err);
-        return err;
+        return reject(err);
       } else { //eslint-disable-line
-          // console.log('directory Created');
-        return status;
+        return resolve('success');
       }
     });
   }
 }
 
-function pitching() {
+
+function pitching(resolve, reject) {
   let ErrorLog = [];
   let pH = 0;
 
-  let stream = IbuErrorDoc.find({})
+  const stream = IbuErrorDoc.find({})
     .select('filename filePathXML filePathIMG XMLerrors IMGerrors').stream();
-  stream.on('data', (wort) => {
 
+  stream.on('data', (wort) => {
     if ((wort.XMLerrors.length <= 0) || (wort.IMGerrors.length <= 0)) {
       if ((wort.filePathXML !== '') && (wort.filePathIMG !== '')) {
         // Passing!
       } else {
-        let missing = (wort.filePathXML==='' ? 'MODS' : 'Image');
+        const missing = (wort.filePathXML === '' ? 'MODS' : 'Image');
         ErrorLog.push(`${String(wort.filename)} missing${missing}`);
-        pH++; // pH level is high!
+        pH++;
       }
     } else {
       ErrorLog.push(`${String(wort.filename)} ${wort.XMLerrors} ${wort.IMGerrors}`);
       pH++;
-      // pH level is high!
-      // Construct an array of List[fileNames][errors]
-      // Run Status();
     }
   }).on('error', (err) => {
-    console.log(err);
+    return reject(err);
   }).on('close', () => {
-    console.log(ErrorLog);
+    let message;
     if (pH > 0) {
-      console.log('pH level is too high: Status fired!');
+      // message = reject(Error(ErrorLog));
+      message = 'success';
     } else {
-      // Go to next function
+      message = 'success';
     }
-  });
-}
-function controller() {
-  abduction(gravity);
-  pitching();
-  ingestionPrep();
-  // ingestion(target,parentpid,namespace,model);
+  }).then(resolve(message));
 }
 
+// ingestion(target,parentpid,namespace,model);
+
+function controller() {
+  abduction(gravity);
+  Promise.race([pitching])
+  .then(console.log)
+  .catch(function(num) { console.log('catch: ', num); });
+}
 
 export default controller;
