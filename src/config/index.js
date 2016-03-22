76a287f0a5a9cc760fa0d4bb3d2aed515d3a8cef
Don Richards <donrichards@outlook.com>
2016-03-22 16:59:32 -0400

    Modularize config Function
    Add params outside of core code

---
+++ src/config/index.js
@@ -0,0 +1,18 @@
+
+const path = require('path');
+// const rootPath = path.normalize(__dirname + '/test/');
+// const rootPath = path.normalize('./test/');
+const rootPath = './test';
+// const rootPath = path.normalize('./test');
+module.exports = {
+  development: {
+    rootPath,
+    database: 'mongodb://localhost/express-todo',
+    port: process.env.PORT || 3000,
+  },
+  production: {
+    rootPath,
+    database: 'mongodb://jasonshark:multivision@ds037478.mongolab.com:37478/multivision',
+    port: process.env.PORT || 80,
+  },
+};
