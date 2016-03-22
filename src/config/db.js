37a244940a446e402e31a626015daf2c62dece7e
Don Richards <donrichards@outlook.com>
2016-03-22 16:58:42 -0400

    Modify config
    adjust functionality for promises

--- src/config/db.js
+++ src/config/db.js
@@ -1,28 +1,34 @@
 'use strict';
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+// mongoose.Promise = require('bluebird');
+const Promise = require('bluebird');
+Promise.promisifyAll(require('mongoose'));
 
-let config = {
-  'db': 'fileList',
-  'host': 'localhost',
-  'port': 27017
+const config = {
+  db: 'fileList',
+  host: 'localhost',
+  port: 27017
 };
 
 const dbUri = 'mongodb://localhost:27017/ibu';
 
 mongoose.connect(dbUri);
 
-mongoose.connection.on('connected', function () {
+mongoose.connection.on('connected', () => {
+  // console.log('Connected to DB,,, Now what?');
 });
-mongoose.connection.on('error',function (err) {
-  console.err(err.stack);
+
+mongoose.connection.on('error', (err) => {
+  // console.log('Stack error in db: ', err);
 });
-mongoose.connection.on('disconnected', function () {
-  console.log('Mongoose default connection disconnected');
+
+mongoose.connection.on('disconnected', () => {
+  // console.log('Mongoose default connection disconnected');
 });
 
 // If the Node process ends, close the Mongoose connection
-process.on('SIGINT', function() {
-  mongoose.connection.close(function () {
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
     console.log('Mongoose default connection disconnected through app termination');
     process.exit(0);
   });
